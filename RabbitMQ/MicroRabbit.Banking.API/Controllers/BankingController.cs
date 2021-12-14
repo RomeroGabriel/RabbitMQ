e1ba6e815609bf90d058b177df98a87ef640426f
RomeroGabriel <gabrielromerods@hotmail.com>
2021-12-14 19:53:48 -0300

    API

---
+++ RabbitMQ/MicroRabbit.Banking.API/Controllers/BankingController.cs
@@ -0,0 +1,29 @@
+﻿using MicroRabbit.Baking.Application.Interfaces;
+using MicroRabbit.Baking.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Banking.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BankingController
+    {
+        private readonly IAccountServices accountService;
+
+        public BankingController(IAccountServices accountService)
+        {
+            this.accountService = accountService;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Account>> Get()
+        {
+            return Ok(accountService.GetAccounts());
+        }
+    }
+}
