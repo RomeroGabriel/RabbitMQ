4e164601dcf866b1557fe0baaae599cd097b21df
RomeroGabriel <gabrielromerods@hotmail.com>
2021-12-14 19:53:38 -0300

    Service

---
+++ RabbitMQ/MicroRabbit.Baking.Application/AccountServices/AccountServices.cs
@@ -0,0 +1,22 @@
+﻿using MicroRabbit.Baking.Application.Interfaces;
+using MicroRabbit.Baking.Domain.Interfaces;
+using MicroRabbit.Baking.Domain.Models;
+using System.Collections.Generic;
+
+namespace MicroRabbit.Baking.Application.AccountServices
+{
+    public class AccountServices : IAccountServices
+    {
+        private readonly IAccountRepository accountRepository;
+
+        public AccountServices(IAccountRepository accountRepository)
+        {
+            this.accountRepository = accountRepository;
+        }
+
+        public IEnumerable<Account> GetAccounts()
+        {
+            return accountRepository.GetAccounts();
+        }
+    }
+}
---
+++ RabbitMQ/MicroRabbit.Baking.Application/Interfaces/IAccountServices.cs
@@ -0,0 +1,10 @@
+﻿using MicroRabbit.Baking.Domain.Models;
+using System.Collections.Generic;
+
+namespace MicroRabbit.Baking.Application.Interfaces
+{
+    public interface IAccountServices
+    {
+        IEnumerable<Account> GetAccounts();
+    }
+}
