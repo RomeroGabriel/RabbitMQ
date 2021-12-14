e4d984365da9ecd7e87879047d6d6aa96e789d45
RomeroGabriel <gabrielromerods@hotmail.com>
2021-12-14 19:53:25 -0300

    Data class

---
+++ RabbitMQ/MicroRabbit.Baking.Data/Context/BakingDbContext.cs
@@ -0,0 +1,24 @@
+﻿using MicroRabbit.Baking.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MicroRabbit.Baking.Data.Context
+{
+    public class BakingDbContext : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+
+        public BakingDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public BakingDbContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-FECGN83;Database=BankingDB;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
---
+++ RabbitMQ/MicroRabbit.Baking.Data/Repository/AccountRepository.cs
@@ -0,0 +1,22 @@
+﻿using MicroRabbit.Baking.Data.Context;
+using MicroRabbit.Baking.Domain.Interfaces;
+using MicroRabbit.Baking.Domain.Models;
+using System.Collections.Generic;
+
+namespace MicroRabbit.Baking.Data.Repository
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private BakingDbContext ctx;
+
+        public AccountRepository(BakingDbContext ctx)
+        {
+            this.ctx = ctx;
+        }
+
+        public IEnumerable<Account> GetAccounts()
+        {
+            return ctx.Accounts;
+        }
+    }
+}
